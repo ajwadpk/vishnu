236a4cccdd743c41d7db0305413161c08d5c163d
Ajwad <156331426+ajwadpk@users.noreply.github.com>
2024-08-14 19:28:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Programmer", "Photographer", "Music Producer", "Nature Lover"],
+        strings: ["Content Creator", "Skilled", "Innovative", "Creative"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Programmer", "Photographer", "Music Producer", "Nature Lover"],
+        strings: ["Content Creator", "Skilled", "Innovative", "Creative"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
